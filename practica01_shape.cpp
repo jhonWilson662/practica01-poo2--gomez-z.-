78a1f743c90379023f3f9a78cc536ec341082e21
jhon wilson <jhongomezzapana@gmail.com>
2025-09-15 21:53:49 -0500

    Update practica01_shape.cpp

--- practica01_shape.cpp
+++ practica01_shape.cpp
@@ -4,37 +4,32 @@
 #include <memory>
 #include <cmath>
 using namespace std;
-
 struct Shape {
-    virtual ~Shape() = default;
-    virtual double area() const = 0;
-    virtual double perimeter() const = 0;
+ virtual ~Shape() = default;
+ virtual double area() const = 0;
+ virtual double perimeter() const = 0;
 };
-
 class Rectangle : public Shape {
-    double width_, height_;
+ double w_, h_;
 public:
-    Rectangle(double w, double h) : width_(w), height_(h) {}
-    double area() const override { return width_ * height_; }
-    double perimeter() const override { return 2 * (width_ + height_); }
+ Rectangle(double w, double h) : w_(w), h_(h) {}
+ double area() const override { return w_ * h_; }
+ double perimeter() const override { return 2 * (w_ + h_); }
 };
-
 class Circle : public Shape {
-    double radius_;
+ double r_;
 public:
-    explicit Circle(double r) : radius_(r) {}
-    double area() const override { return M_PI * radius_ * radius_; }
-    double perimeter() const override { return 2 * M_PI * radius_; }
+ explicit Circle(double r) : r_(r) {}
+ double area() const override { return M_PI * r_ * r_; }
+ double perimeter() const override { return 2 * M_PI * r_; }
 };
-
 int main() {
-    vector<unique_ptr<Shape>> shapes;
-    shapes.emplace_back(make_unique<Rectangle>(4.0, 3.0));
-    shapes.emplace_back(make_unique<Circle>(2.5));
-
-    for (const auto& s : shapes) {
-        cout << "Área = " << s->area()
-             << " | Perímetro = " << s->perimeter() << '\n';
-    }
-    return 0;
+ vector<unique_ptr<Shape>> shapes;
+ shapes.emplace_back(make_unique<Rectangle>(4.0, 3.0));
+ shapes.emplace_back(make_unique<Circle>(2.5));
+ for (const auto& s : shapes) {
+ cout << "A=" << s->area() << " | P=" << s->perimeter() <<
+'\n';
+ }
+ return 0;
 }
